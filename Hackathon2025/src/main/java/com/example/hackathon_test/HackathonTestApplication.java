24ef005bfde9fc26ea80ffd64a347b78a79feed7
Samuel <samuelanu06@gmail.com>
2025-11-22 13:24:59 +0000

    New project

---
+++ Hackathon2025/src/main/java/com/example/hackathon_test/HackathonTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonTestApplication.class, args);
+    }
+
+}
---
+++ Hackathon2025/src/test/java/com/example/hackathon_test/HackathonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
